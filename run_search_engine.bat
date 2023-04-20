727cef29d004a9dbf8cae02cf15893770bec0510
Hastimazadeh <81362792+Hastimazadeh@users.noreply.github.com>
2023-04-21 00:47:06 +0200

    Update run_search_engine.bat

--- run_search_engine.bat
+++ run_search_engine.bat
@@ -1,1 +1,1 @@
-java -cp classes -Xmx1g ir.Engine -d "C:\Users\HP\Documents\courses\Search Engines and Information Retrieval systems - DD2477\Assignments\davisWiki" -l ir22.png -p patterns.txt
+java -cp classes -Xmx1g ir.Engine -d "dir of davisWiki" -l ir22.png -p patterns.txt
